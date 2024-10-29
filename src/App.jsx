82703bddf5262bcbe4980dfcf2c86db04d25237e
Jodie Stead <jodie.stead1@gmail.com>
2024-10-29 13:35:08 +0000

    Added NavLink to Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./Pages/HomePage"
 import Header from "./Components/Header"
-import Nav from "./Components/Nav"
 
 function App () {
     return (
--- src/Components/NavLinks/index.jsx
+++ src/Components/NavLinks/index.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom"
+
 function NavLinks() {
+    
+
     return(
         <div>
             <h2>New Email </h2>
-            <h2>Inbox </h2>
+            <Link to="/">Inbox</Link>
             <h2>Sent </h2>
             <h2>Deleted </h2>
         </div>

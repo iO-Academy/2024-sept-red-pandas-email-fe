6c8a9588a7c9e2361357113ad5d948658944918f
Viv Hayles <vivhayles@gmail.com>
2024-10-29 11:44:47 +0000

    Made a fetch request for emails and to get their id

---
+++ src/Components/OpenEmail/index.jsx
@@ -0,0 +1,15 @@
+function OpenEmail({id, sender, subject, body}) {
+    return (
+        <section>
+            <h3>Email id: {id}</h3>
+            <ul>
+                <li>Sender: {email}</li>
+                <li>Subject: {subject}</li>
+                <li>Body: {body}</li>
+            </ul>
+        </section>
+        )
+    }
+
+
+export default OpenEmail
--- src/Components/OpenEmails/index.jsx
+++ src/Components/OpenEmails/index.jsx
@@ -1,22 +1,33 @@
 import { useEffect, useState } from "react"
+import OpenEmail from "../OpenEmail" 
 
-function OpenEmails (){
+function OpenEmails(){
 
     const [emails, setEmails] = useState ([])
-useEffect (() => {
-    fetch('https://email-client-api.dev.io-academy.uk/emails?search=example search term')
-  .then(res => res.json())
-  .then(data => {
-    console.log(data.emails)
 
-
+    useEffect (() => {
+    fetch('https://email-client-api.dev.io-academy.uk/emails')
+    .then(res => res.json())
+    .then(data => {
+      setEmails(data.data)
   })
-})
+},[])
+
 return (
     <div>
-
+        <h2>Email List</h2>
+        <ul>
+            {emails.map((email) => (
+                <li key={email.id}>
+                    <h3>From: {email.email}</h3>
+                    <p>ID: {email.id}</p>
+                    <p>Subject: {email.subject}</p>
+                    <p>{email.body}</p>
+                </li>
+            ))}
+        </ul>
     </div>
-)
-
+  )
 }
+
 export default OpenEmails

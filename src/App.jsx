9b3f177f72c1936b9e22807994b8726db3c22b23
Jodie Stead <jodie.stead1@gmail.com>
2024-10-29 11:06:58 +0000

    added Nav button and modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./Pages/HomePage"
+import Header from "./Components/Header"
+import Nav from "./Components/Nav"
 
 function App () {
     return (
         <div>
             
            <BrowserRouter>
+                <Header />
                 <Routes>
                     <Route path="/" element={<HomePage/>} />
             
--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react"
+import NavLinks from "../NavLinks"
+
 function Nav() {
-    return(
-        <div>
-            <button>MENU</button>
-        </div>
-    )
+
+   const [modalOpen, setModalOpen] = useState(false)
+
+   function handleClick() {
+    setModalOpen(!modalOpen)
+   }
+   return (
+    <div>
+        <button onClick={handleClick}>{modalOpen ? '' : ''} MENU</button>
+        {modalOpen && <NavLinks />} 
+    </div>
+   )
+
 }
 
 export default Nav

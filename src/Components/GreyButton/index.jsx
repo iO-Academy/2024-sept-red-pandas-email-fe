3ccdac7a174c54dc443c930afe46cb52bebc0666
Michael Day <michaelday1991@hotmail.co.uk>
2024-10-30 11:56:38 +0000

    Finalised changes to Grey Button

--- src/Components/GreyButton/index.jsx
+++ src/Components/GreyButton/index.jsx
@@ -3,11 +3,9 @@ import { Link } from "react-router-dom"
 
 function GreyButton ({link, text}) {
     return (
-        <div className="grid justify-items-end pt-4">
             <Link to={link} className="px-4 py-1.5 bg-gray-500 text-white rounded hover:bg-gray-600">
             {text}
             </Link>
-        </div>
     )
 }
 export default GreyButton

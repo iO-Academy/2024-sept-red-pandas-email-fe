daa74d85538df43a6623a5b920e6900e3c723b84
Dan Thomas <danthomas88@hotmail.com>
2024-10-30 12:10:33 +0000

    s2task5-fetch Inbox Summary and Email Summary

--- src/Components/EmailSummary/index.jsx
+++ src/Components/EmailSummary/index.jsx
@@ -1,20 +1,23 @@
-import { Link } from "react-router-dom"
+function EmailSummary ({email, name, subject, body, date}) {
 
-function EmailSummary ({name, subject, body, date}) {
 
     return (
-        <div>
-            {/* <Link to="/emails/:id"> */}
-                <h4>{name}</h4>
-                <p>{subject}</p>
-                <p>{body}</p>
-                <p>{date}</p>
-            {/* </Link> */}
-          
+        <div className="p-4 border-b border-gray-400 border-solid flex justify-between h-25">
+           <div>
+            <h4 className="text-lg font-bold ">{name}</h4>
+            <p className="font-normal">{subject}</p>
+            <p>{body.slice(0, 22) + "..."}</p>
+            </div>
+            <div className="text-right w-40 font-bold">
+            <p>{date.split(" ")[0]}</p>
+            </div>
         </div>
     )
 }
 
+
 export default EmailSummary
+
+
 
 

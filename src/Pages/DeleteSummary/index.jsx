b4ac1b28c06dd59eba5513d52bbc82790585fc58
Oksana Bakhaieva <obahaeva@gmail.com>
2024-10-31 13:53:40 +0000

    Fixed DeleteButton

--- src/Pages/DeleteSummary/index.jsx
+++ src/Pages/DeleteSummary/index.jsx
@@ -1,29 +1,13 @@
-function DeleteSummary() {
 
 
-        return (
-            <div>
+function DeleteSummary () {
+  
 
-            </div>
+    return (
+    <div>
 
-
-
-            // <div className="pt-16">
-            //     {emails.map(email => {
-            //         return (
-            //             <EmailSummary  
-            //                 key={email.id}
-            //                 id={email.id}
-            //                 name={email.name}
-            //                 subject={email.subject}
-            //                 body={email.body}
-            //                 date={email.date_created}
-            //                 read={email.read}
-            //             />
-            //         )
-            //     })}
-            // </div>
-        )
+    </div>
+    )
 }
 
 export default DeleteSummary

8a3508fc39cd6d404ff6ace6d0fec5a80f2a7d7b
Jodie Stead <jodie.stead1@gmail.com>
2024-10-31 14:29:36 +0000

    Finished the DeleteButton and DeleteSummary

--- src/Pages/DeleteSummary/index.jsx
+++ src/Pages/DeleteSummary/index.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from "react"
+import EmailSummary from "../../Components/EmailSummary"
 
 
 function DeleteSummary () {
-  
+    
+    const [emailsDeleted, setEmailsDeleted] = useState([])
+
+    function getEmailsDeleted() {
+        fetch('https://email-client-api.dev.io-academy.uk/emails/deleted')
+        .then(res => res.json())
+        .then(data => {
+            setEmailsDeleted(data.data)
+            console.log(data)
+        })
+    }
+
+    useEffect(getEmailsDeleted, [])
 
     return (
-    <div>
-
-    </div>
+        <div className="pt-16">
+            {emailsDeleted.map(emailsDeleted => {
+                return (
+                    <EmailSummary  
+                        key={emailsDeleted.id}
+                        id={emailsDeleted.id}
+                        name={emailsDeleted.name}
+                        subject={emailsDeleted.subject}
+                        body={emailsDeleted.body}
+                        date={emailsDeleted.date_created}
+                        read={emailsDeleted.read}
+                    />
+                )
+            })}
+        </div>
     )
 }
 

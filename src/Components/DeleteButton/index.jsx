80d98b248db37d6ddd9e7c118e1e1630e5afb7af
Jodie Stead <jodie.stead1@gmail.com>
2024-10-31 14:57:47 +0000

    changes made to refreshing delete email

--- src/Components/DeleteButton/index.jsx
+++ src/Components/DeleteButton/index.jsx
@@ -1,7 +1,9 @@
-import { Link, useParams } from "react-router-dom"
+import {useNavigate, useParams } from "react-router-dom"
 
 function DeleteButton({link, text}) {
     const { id } = useParams()
+
+    const navigate = useNavigate()
     
     function handleClickDelete() {
         fetch(`https://email-client-api.dev.io-academy.uk/emails/${id}`, 
@@ -9,15 +11,15 @@ function DeleteButton({link, text}) {
             .then(res => res.json())
             .then((data) => {
                 console.log(data)
-                
+                navigate(link)
         })}
 
     return (
         <div>
            
-            <Link onClick={handleClickDelete} to={link} className="px-4 py-1.5 bg-red-600 text-white rounded hover:bg-gray-600">
+            <button onClick={handleClickDelete} className="px-4 py-1.5 bg-red-600 text-white rounded hover:bg-gray-600">
                 {text}
-            </Link>
+            </button>
         </div>
     )
 }
